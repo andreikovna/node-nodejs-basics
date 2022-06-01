876a44e9f77f00e2bee673adb6fb0b9bc0c29d7a
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-01 22:53:36 +0300

    feat: add create.js and copy.js solutions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,54 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs";
+import * as path from "path";
+
+const fsPromises = fs.promises;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const destination = path.join(__dirname, "files-copy");
+const source = path.join(__dirname, "files");
+
 export const copy = async () => {
-    // Write your code here 
+  function copyFiles() {
+    fs.mkdir(destination, { recursive: true }, (err) => {
+      if (err) {
+        throw new Error("FS operation failed");
+      }
+      fs.readdir(source, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        } else {
+          files.forEach((file) => {
+            const fileName = file.name.toString();
+
+            fsPromises
+              .copyFile(
+                path.join(__dirname, "files", fileName),
+                path.join(__dirname, "files-copy", fileName)
+              )
+              .catch(function (error) {
+                throw new Error("FS operation failed");
+              });
+          });
+        }
+      });
+    });
+  }
+
+  fs.access(source, function (error) {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.access(destination, function (error) {
+        if (error) {
+          copyFiles();
+        } else {
+          throw new Error("FS operation failed");
+        }
+      });
+    }
+  });
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from 'fs';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+    const text = 'I am fresh and young';
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    fs.writeFile(filePath, text, { flag: 'wx' }, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    })
 };
