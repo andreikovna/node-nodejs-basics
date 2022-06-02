c3a3d8835cad8a8a58cafe1cb222f45063761005
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-02 23:39:38 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,21 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const { stdout } = process;
+const fileToRead = path.join(__dirname, "files", "fileToRead.txt");
+
 export const read = async () => {
-    // Write your code here 
+  fs.access(fileToRead, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      const readStream = fs.createReadStream(fileToRead, "utf-8");
+      readStream.on("data", (chunk) => stdout.write(chunk));
+    }
+  });
 };
