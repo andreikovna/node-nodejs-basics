fec7fffc03b756c30d75592c0345a6afb6e810a3
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-02 23:01:26 +0300

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const removePath = path.join(__dirname, "files", "fileToRemove.txt");
+
 export const remove = async () => {
-    // Write your code here 
+  fs.access(removePath, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.rm(removePath, (error) => {
+        if (error) {
+          throw new Error("FS operation failed");
+        }
+      });
+    }
+  });
 };
