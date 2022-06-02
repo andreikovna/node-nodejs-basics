14e8713c518b3786ba43092fc3c4f267e0d40759
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-02 23:23:08 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const source = path.join(__dirname, "files");
+
 export const list = async () => {
-    // Write your code here 
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      const resultFiles = [];
+      files.map((file) => resultFiles.push(file.name));
+      console.log("\nCurrent directory filenames:\n", resultFiles);
+    }
+  });
 };
