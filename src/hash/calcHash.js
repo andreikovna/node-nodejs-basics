2ec6beb789ec7991dfc82afe05c125fd70fc404d
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-05 00:28:01 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { promises as fs } from "fs";
+import * as path from "path";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const { stdout } = process;
+const fileToRead = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const fileResult = await fs.readFile(fileToRead, "utf8");
+  const result = createHash("sha256").update(fileResult).digest("hex");
+  stdout.write(result);
 };
