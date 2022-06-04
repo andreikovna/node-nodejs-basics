629f9cbb0423d073aebe42bd9ad12e37e0647492
Daria Litvinova <d.andreikovna@gmail.com>
2022-06-05 00:05:15 +0300

    feat: add args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2);
+    args.forEach((argument, index) => {
+        if (index === 0) {
+            console.log(`propName is ${argument}`);
+        } else console.log(`prop${index+1}Name is ${argument}`);
+    })
+  };
