239d84078451f47ee1599e0155cd79aefc941b49
= <litvinonada@solit.by>
2022-11-17 15:36:45 +0300

    fix: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import * as fs from "fs";
+import { promises as fs } from 'fs';
 import * as path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,11 +9,11 @@ const __dirname = dirname(__filename);
 const create = async () => {
   const text = "I am fresh and young";
   const filePath = path.join(__dirname, "files", "fresh.txt");
-  fs.writeFile(filePath, text, { flag: "wx" }, (err) => {
-    if (err) {
-      throw new Error("FS operation failed");
-    }
-  });
+  try {
+    await fs.writeFile(filePath, text, { flag: "wx" })
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
