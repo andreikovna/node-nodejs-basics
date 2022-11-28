8133010e3fcb4ee6037110d61aa26dfd6ca87b8d
= <litvinonada@solit.by>
2022-11-28 10:19:59 +0300

    refactor:add error

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,6 +4,7 @@ const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2
 
 const sendResult = () => {
     try {
+        if (Math.random() > 0.5) throw new Error('error');
         parentPort.postMessage({status: 'resolved', data: nthFibonacci(workerData)});
     } catch (err) {
         parentPort.postMessage({status: 'error', data: null});
