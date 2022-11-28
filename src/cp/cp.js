fccc6c889546d47a4d746eaa6f64f941358a72ff
= <litvinonada@solit.by>
2022-11-28 11:10:10 +0300

    feat: add child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,24 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as path from "path";
+import { spawn } from "child_process";
+
+const { stdin, stdout } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scriptPath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn("node", [scriptPath, ...args]);
+
+  stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  })
+
+  childProcess.stdout.on('data', (data) => {
+    stdout.write(data);
+  })
 };
 
-spawnChildProcess();
+spawnChildProcess(['hello', 'world', 'I am', 'happy']);
